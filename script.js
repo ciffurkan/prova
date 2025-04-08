68904320b8e8e9fc6820051f46c8fc5d4449c299
ciffurkan <ciftepalafurkan@gmail.com>
2025-04-08 11:37:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Play() {
+    document.getElementById("play-button").addEventListener("click", function () {
+        window.location.href = "partita.html"; // Cambia con il tuo URL
+    });
+}
+
+Play();
