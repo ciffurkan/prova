d2dda8f74b07c73f06159c724d409ae7d0a26a92
ciffurkan <ciftepalafurkan@gmail.com>
2025-05-17 18:07:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function Play() {
-    document.getElementById("play-button").addEventListener("click", function () {
-        window.location.href = "partita.html";
-    });
-}
+const playButton = document.getElementById("play-button");
+const optionsPopup = document.getElementById("options-popup");
+const closePopupButton = document.getElementById("close-popup-button");
+const option1Button = document.getElementById("option1-button");
+const option2Button = document.getElementById("option2-button");
+const option3Button = document.getElementById("option3-button");
 
-Play();
+playButton.addEventListener("click", function () {
+    optionsPopup.style.display = "flex"; // Mostra il popup
+});
+
+closePopupButton.addEventListener("click", function () {
+    optionsPopup.style.display = "none"; // Nasconde il popup
+});
+
+option1Button.addEventListener("click", function () {
+    sessionStorage.setItem("difficolta", "facile");
+    sessionStorage.setItem('buttonClicked', 'true');
+    window.location.href = "partita.html";
+    optionsPopup.style.display = "none";
+});
+
+option2Button.addEventListener("click", function () {
+    sessionStorage.setItem("difficolta", "media");
+    sessionStorage.setItem('buttonClicked', 'true');
+    window.location.href = "partita.html";
+    optionsPopup.style.display = "none";
+});
+
+option3Button.addEventListener("click", function () {
+    sessionStorage.setItem("difficolta", "difficile");
+    sessionStorage.setItem('buttonClicked', 'true');
+    window.location.href = "partita.html";
+    optionsPopup.style.display = "none";
+});
